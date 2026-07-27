ef09d81245c4976a5bbf6456ab56c1d82f6f2eab
Rafael <rafael@gmail.com>
2026-07-27 12:00:40 +0100

    Agregar configuración de internacionalización con soporte para español e inglés

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,8 +4,16 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
-    devToolbar:{enabled:false},
-      vite: {
+  devToolbar: { enabled: false },
+  vite: {
     plugins: [tailwindcss()],
   },
+  i18n: {
+    defaultLocale: 'es',
+    locales: ['es','en'],
+    routing: {
+      prefixDefaultLocale: true,
+    }
+  }
+
 });
